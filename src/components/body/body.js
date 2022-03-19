d5f03b22abb635ea3cfa298d951eb7a1a1303c4b
Matt-Fontes <Matt-Fontes@users.noreply.github.com>
2022-03-19 16:02:24 -0300

    Add request para api de receitas, usando axios.

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,17 +1,35 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import './body.css';
 import Card from "../card/card";
+import axios from "axios";
 
 function Body(){
+
+    const [recipes, setRecipes] = useState([]);
+
+
+    useEffect(() => {
+        // Fetching as receitas
+        const apiUrl = 'https://recipes-api-appmoove.herokuapp.com/menu';
+        axios.get(apiUrl, {headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+          }}).then((repos) => {
+          const allRepos = repos.data;
+          console.log(repos.data);
+          setRecipes(repos.data);
+        });
+    }, [setRecipes]);
+
     return (
         <div className="bodyContainer">
             <div className="bodyTitle">Receitas</div>
             <div className="cardsContainer">
-                <Card name="Crock Pot Roast"></Card>
-                <Card name="Crock Pot Roast"></Card>
-                <Card name="Crock Pot Roast"></Card>
-                <Card name="Crock Pot Roast"></Card>
-                <Card name="Crock Pot Roast"></Card>
+                {
+                    recipes.map(el => (
+                        <Card name={el.name} image={el.imageURL}/>
+                    ))
+                }
             </div>
         </div>
     );
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,10 +1,44 @@
-import React from "react";
+import {React, useEffect, useState} from "react";
 import './card.css';
+import imgPlaceHolder from '../../images/notFound.png';
 
 function Card(props){
+
+    const [img, setImg] = useState(imgPlaceHolder);
+
+    function checkIfImageExists(url, callback) {
+        const img = new Image();
+    
+        img.src = url;
+    
+        if (img.complete) {
+          callback(true);
+        } else {
+          img.onload = () => {
+            callback(true);
+          };
+          
+          img.onerror = () => {
+            callback(false);
+          };
+        }
+    }
+
+    useEffect(() => {
+        checkIfImageExists(props.image, (exists) => {
+            console.log('rodando: ' + props.image);
+            if (exists) {
+                setImg(props.image);
+            } else {
+                console.log('FAIL:' + props.image)
+            }
+        });
+    }, []);
+    
+
     return(
         <div className="cardContainer">
-            <div className="recipeImg"></div>
+            <div className="recipeImg" style={{backgroundImage: 'url(' + img + ')' }}></div>
             <div className="recipeName">{props.name}</div>
             <div className="seeRecipe">Ver Receita</div>
         </div>
