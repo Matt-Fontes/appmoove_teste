962b271ce98149900577ff52faa91447ae7ee739
Matt-Fontes <Matt-Fontes@users.noreply.github.com>
2022-03-19 00:57:12 -0300

    Header design, font "montserrat" e icones adicionados

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './header.css';
+import '../../fontawesome/css/all.css'; // Icones
+import '../../fontawesome/alt/css/all.css';
 
 function Header(){
     return(
         <div className="headerContainer">
-            
+            <div className="middleContainer">
+                <span className="slogan">Encontre a receita que procura</span>
+                <div className="searchBoxContainer">
+                    <input type="text" className='searchBox' placeholder='Buscar Receita'/>
+                    <i class="far fa-search searchIcon"></i>
+                </div>
+            </div>
         </div>
     );
 }
